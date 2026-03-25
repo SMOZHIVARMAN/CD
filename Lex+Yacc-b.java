1fc3c92f69da6eef32901827e1674caeb6c9e507
SMOZHIVARMAN <mozhivarman2005@gmail.com>
2026-03-25 11:13:15 +0530

    Experiment 5.b

--- Lex+Yacc-b.java
+++ Lex+Yacc-b.java
@@ -1,27 +1,15 @@
 import java.util.*;
 
-public class ExpressionEvaluator {
+public class VariableCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter expression: ");
-        String expr = sc.nextLine();
+        System.out.print("Enter variable: ");
+        String var = sc.next();
 
-        try {
-            double result = eval(expr);
-            System.out.println("Result = " + result);
-        } catch(Exception e) {
-            System.out.println("Invalid Expression");
-        }
-    }
-
-    // Simple evaluator using Java Script Engine
-    public static double eval(String expr) throws Exception {
-        return new javax.script.ScriptEngineManager()
-                .getEngineByName("JavaScript")
-                .eval(expr) instanceof Number
-                ? ((Number)new javax.script.ScriptEngineManager()
-                .getEngineByName("JavaScript")
-                .eval(expr)).doubleValue() : 0;
+        if(var.matches("[a-zA-Z][a-zA-Z0-9]*"))
+            System.out.println("Valid Variable");
+        else
+            System.out.println("Invalid Variable");
     }
 }
